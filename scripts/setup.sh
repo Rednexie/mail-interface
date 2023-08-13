c7a32ec187bfb18ac03a909be8018da79bcd836c
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-08-13 22:49:45 +0300

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo https://github.com/Rednexie/mailer-template
+echo
+echo Welcome to mailer-template installer for Windows.
+echo "This script will install the dependencies and help you to setup your server."
+echo "Starting to install..."
+echo
+echo ------------------------------------------------------------------------------
+if command -v which node >/dev/null 2>&1; then
+  echo "node.js runtime environment is installed. continuing..."
+else
+  echo "node.js runtime environment is not installed, quitting..."
+  read -p "Press enter to continue"
+  exit 1
+fi
+
+if command -v which npm >/dev/null 2>&1; then
+  echo "node package manager(npm) is installed, continuing..."
+else
+  echo "node package manager(npm) is not installed, quitting..."
+  read -p "Press enter to continue"
+  exit 1
+fi
+
+npm i consoled.js@1.0.4 express fs express-rate-limit node-fetch@2.6.1
+echo ------------------------------------------------------------------------------
+echo
+echo "Installation is done."
+echo "Thanks for using mailer-template"
