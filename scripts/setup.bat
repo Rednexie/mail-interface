b2134319e511ec0f7c0cb918159a32de4b9e1f00
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-08-27 00:49:24 +0300

    Update setup.bat

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -2,29 +2,29 @@
 color 0f
 echo https://github.com/Rednexie/mail-interface
 echo.
-echo Welcome to mail-interface installer for Windows.
-echo This script will install the dependencies and help you to setup your server.
-echo Starting to install...
+echo "Welcome to mail-interface installer for Windows."
+echo "This script will install the dependencies and help you to setup your server."
+echo "Starting to install..."
 echo.
-echo ------------------------------------------------------------------------------
+echo "------------------------------------------------------------------------------"
 where node >nul 2>&1
 if %errorlevel% equ 0 (
-  echo node.js runtime environment is installed. continuing...
+  echo "node.js runtime environment is installed. continuing..."
 ) else (
-  echo node.js runtime environment is not installed, quitting...
+  echo "node.js runtime environment is not installed, quitting..."
   pause
   pause
 )
 where npm >nul 2>&1
 if %errorlevel% equ 0 (
-  echo node package manager(npm) is installed, continuing...
+  echo "node package manager(npm) is installed, continuing..."
 ) else (
-  echo node package manager(npm) is not installed, quitting...
+  echo "node package manager(npm) is not installed, quitting..."
   pause
 )
 
 npm i consoled.js@1.0.4 express fs express-rate-limit node-fetch@2.6.1
-echo ------------------------------------------------------------------------------
+echo "------------------------------------------------------------------------------"
 echo.
-echo Installation is done.
-echo Thanks for using mail-interface
+echo "Installation is done."
+echo "Thanks for using mail-interface"
