9c0cf47cefca8c86a34c0e1c067ae3f57842e82f
Rednexie <126991413+Rednexie@users.noreply.github.com>
2025-02-14 20:53:44 +0300

    Create zorin.js

---
+++ modules/zorin.js
@@ -0,0 +1,21 @@
+fetch("https://mj.emailclick.zoringroup.com/wgt/xk5ij/4p2/subscribe?c=f0dab3e5", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "content-type": "application/json",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"99\", \"Google Chrome\";v=\"133\", \"Chromium\";v=\"133\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-storage-access": "active"
+  },
+  "referrer": "https://mj.emailclick.zoringroup.com/wgt/xk5ij/4p2/form?c=b00e855d",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"Email\":\"magneticpurple@rustyload.com\",\"Fields\":[]}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
